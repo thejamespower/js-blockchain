09dacbd6f5b22a57280feb9f6c325ce8f22563ba
James Power <thejamespower@gmail.com>
2021-06-07 16:48:31 +0100

    GET /mine
    - can mine a new block, with hardcoded reward
    - add uuid for node address

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
 const blockchain = require('./blockchain');
 const transaction = require('./transaction');
+const mine = require('./mine');
 
 routes.use('/blockchain', blockchain);
 routes.use('/transaction', transaction);
+routes.use('/mine', mine);
 
 module.exports = routes;
---
+++ api/routes/mine/index.js
@@ -0,0 +1,45 @@
+const routes = require('express').Router();
+const { v1: uuid } = require('uuid');
+
+const blockchain = require('../../../lib/run');
+
+const reward = 1;
+const nodeAddress = uuid().replace(/-/g, '');
+
+routes.get('/', (req, res) => {
+  const lastBlock = blockchain.getLastBlock();
+  const previousBlockHash = lastBlock.hash;
+
+  const currentBlockData = {
+    transactions: blockchain.pendingTransactions,
+    index: lastBlock.index + 1,
+  };
+
+  const nonce = blockchain.proofOfWork({ previousBlockHash, currentBlockData });
+
+  const hash = blockchain.hashBlock({
+    previousBlockHash,
+    currentBlockData,
+    nonce,
+  });
+
+  // reward
+  // ("00" sender infers mining reward)
+  blockchain.createNewTransaction({
+    amount: reward,
+    sender: '00',
+    recipient: nodeAddress,
+  });
+
+  const newBlock = blockchain.createNewBlock({
+    nonce,
+    previousBlockHash,
+    hash,
+  });
+
+  res.json({
+    block: newBlock,
+  });
+});
+
+module.exports = routes;
