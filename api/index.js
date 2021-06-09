7c3b96eb7ae0fa7210e588b31fb9f092c47b7974
James Power <thejamespower@gmail.com>
2021-06-09 13:07:59 +0100

    Rename PORT -> NODE_PORT

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const handleErrors = require('./middleware/errorHandler');
 
-const PORT = process.env.PORT || 8080;
+const NODE_PORT = process.env.NODE_PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,9 +14,9 @@ app.use('/', routes);
 
 app.use(handleErrors);
 
-app.listen(PORT, () => {
-  if (!process.env.PORT) {
+app.listen(NODE_PORT, () => {
+  if (!process.env.NODE_PORT) {
     console.log('No port specified, default to 8080');
   }
-  console.log(`Node running on port: ${PORT}`);
+  console.log(`Node running on port: ${NODE_PORT}`);
 });
