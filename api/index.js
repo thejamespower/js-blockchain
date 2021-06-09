c0be2b49538a8e63881fddf2ea89011780158234
James Power <thejamespower@gmail.com>
2021-06-09 12:26:58 +0100

    API port variable

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 const handleErrors = require('./middleware/errorHandler');
 
+const PORT = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,4 +14,9 @@ app.use('/', routes);
 
 app.use(handleErrors);
 
-app.listen(8080, () => console.log('Listening on port 8080...'));
+app.listen(PORT, () => {
+  if (!process.env.PORT) {
+    console.log('No port specified, default to 8080');
+  }
+  console.log(`Node running on port: ${PORT}`);
+});
