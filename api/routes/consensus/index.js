354ddf24a78d9bce0daf0bd79343f9449ab5925f
James Power <thejamespower@gmail.com>
2021-06-23 13:58:13 +0100

    /GET consensus

---
+++ api/routes/consensus/index.js
@@ -0,0 +1,62 @@
+const routes = require('express').Router();
+
+const blockchain = require('../../../lib/run');
+const request = require('request-promise-native');
+
+routes.get('/', async (req, res, next) => {
+  try {
+    const blockchains = await Promise.all(
+      blockchain.networkNodes.map((url) =>
+        url
+          ? request({
+              uri: url + '/blockchain',
+              method: 'GET',
+              json: true,
+            })
+          : null
+      )
+    );
+
+    const currentChainLength = blockchain.chain.length;
+    let maxChainLength = currentChainLength;
+    let newLongestChain = null;
+    let newPendingTransactions = null;
+
+    blockchains.forEach(({ chain, pendingTransactions }) => {
+      if (chain.length > maxChainLength) {
+        maxChainLength = chain.length;
+        newLongestChain = chain;
+        newPendingTransactions = pendingTransactions;
+      }
+    });
+
+    if (
+      // if no changes need to be made
+      !newLongestChain ||
+      // there's a new update, but the update is not valid
+      (newLongestChain && !blockchain.chainIsValid(newLongestChain))
+    ) {
+      res.json({
+        status: 'success',
+        message: 'Current chain has not been replaced.',
+        chain: blockchain.chain,
+      });
+    }
+
+    // this node is out of date
+    else if (newLongestChain && blockchain.chainIsValid(newLongestChain)) {
+      blockchain.chain = newLongestChain;
+      blockchain.pendingTransactions = newPendingTransactions;
+
+      res.json({
+        status: 'success',
+        message: 'Current chain has been replaced.',
+        chain: blockchain.chain,
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = routes;
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,11 +4,13 @@ const transaction = require('./transaction');
 const mine = require('./mine');
 const node = require('./node');
 const receiveNewBlock = require('./receive-new-block');
+const consensus = require('./consensus');
 
 routes.use('/blockchain', blockchain);
 routes.use('/transaction', transaction);
 routes.use('/mine', mine);
 routes.use('/node', node);
 routes.use('/receive-new-block', receiveNewBlock);
+routes.use('/consensus', consensus);
 
 module.exports = routes;
